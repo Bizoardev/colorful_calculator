ea6acf7d4ced5cae8ebb4f044081c9a25d378137
Yuki Kaito <ykaito21@gmail.com>
2019-11-29 19:56:44 +0900

    build calculation logic

--- lib/src/ui/screens/calculator_screen.dart
+++ lib/src/ui/screens/calculator_screen.dart
@@ -1,11 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../widgets/button_icon.dart';
-import '../widgets/button_text.dart';
-import '../widgets/calc_row.dart';
+import '../widgets/base_icon.dart';
+import '../widgets/base_text.dart';
+import '../widgets/calc_button.dart';
 
-class CalculatorScreen extends StatelessWidget {
+class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({Key key}) : super(key: key);
+
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String displayNum = '0';
+  String calcNum = '';
+  String operationSymbol = '';
+  double result;
+  bool displaySymbol = false;
+  bool shouldCalculate = false;
+
+  BaseIcon _handleSymbol(String operationSymbol) {
+    switch (operationSymbol) {
+      case '+':
+        return BaseIcon(baseIcon: FontAwesomeIcons.plus);
+      case '-':
+        return BaseIcon(baseIcon: FontAwesomeIcons.minus);
+      case '*':
+        return BaseIcon(baseIcon: FontAwesomeIcons.times);
+      case '/':
+        return BaseIcon(baseIcon: FontAwesomeIcons.divide);
+      default:
+        return null;
+    }
+  }
+
+  void _pressButton(String selectedButton) {
+    setState(() {
+      switch (selectedButton) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+          if (shouldCalculate) {
+            _calculate();
+          } else {
+            result = double.tryParse(calcNum) ?? 0;
+            shouldCalculate = true;
+          }
+          calcNum = '';
+          operationSymbol = selectedButton;
+          displaySymbol = true;
+          break;
+        case '=':
+          _calculate();
+          operationSymbol = '';
+          shouldCalculate = false;
+          break;
+        case 'CE':
+          calcNum = '';
+          displayNum = '0';
+          operationSymbol = '';
+          result = 0;
+          shouldCalculate = false;
+          displaySymbol = false;
+          break;
+        default:
+          calcNum += selectedButton;
+          displayNum = calcNum;
+          displaySymbol = false;
+      }
+    });
+  }
+
+  void _calculate() {
+    switch (operationSymbol) {
+      case '+':
+        result += double.tryParse(calcNum);
+        break;
+      case '-':
+        result -= double.tryParse(calcNum);
+        break;
+      case '*':
+        result *= double.tryParse(calcNum);
+        break;
+      case '/':
+        result /= double.tryParse(calcNum);
+        break;
+      default:
+        break;
+    }
+    calcNum = result.toString();
+    displayNum = calcNum;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +111,22 @@ class CalculatorScreen extends StatelessWidget {
                 ),
                 child: Align(
                   alignment: Alignment.bottomRight,
-                  child: Text(
-                    '0',
-                    style: TextStyle(
-                      fontSize: 80.0,
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      if (displaySymbol)
+                        _handleSymbol(operationSymbol) ?? SizedBox.shrink(),
+                      Flexible(
+                        child: Text(
+                          displayNum,
+                          textAlign: TextAlign.end,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 80.0,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -38,37 +135,98 @@ class CalculatorScreen extends StatelessWidget {
               flex: 2,
               child: Column(
                 children: <Widget>[
-                  CalcRow(
-                    rowContents: [
-                      ButtonIcon(buttonIcon: FontAwesomeIcons.plus),
-                      ButtonIcon(buttonIcon: FontAwesomeIcons.minus),
-                      ButtonIcon(buttonIcon: FontAwesomeIcons.times),
-                      ButtonIcon(buttonIcon: FontAwesomeIcons.divide),
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        CalcButton(
+                          onPressed: () => _pressButton('+'),
+                          buttonWidget:
+                              BaseIcon(baseIcon: FontAwesomeIcons.plus),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('-'),
+                          buttonWidget:
+                              BaseIcon(baseIcon: FontAwesomeIcons.minus),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('*'),
+                          buttonWidget:
+                              BaseIcon(baseIcon: FontAwesomeIcons.times),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('/'),
+                          buttonWidget:
+                              BaseIcon(baseIcon: FontAwesomeIcons.divide),
+                        )
+                      ],
+                    ),
                   ),
-                  CalcRow(
-                    rowContents: [
-                      ButtonText(buttonText: '7'),
-                      ButtonText(buttonText: '8'),
-                      ButtonText(buttonText: '9'),
-                      ButtonText(buttonText: '0'),
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        CalcButton(
+                          onPressed: () => _pressButton('7'),
+                          buttonWidget: BaseText(baseText: '7'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('8'),
+                          buttonWidget: BaseText(baseText: '8'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('9'),
+                          buttonWidget: BaseText(baseText: '9'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('0'),
+                          buttonWidget: BaseText(baseText: '0'),
+                        )
+                      ],
+                    ),
                   ),
-                  CalcRow(
-                    rowContents: [
-                      ButtonText(buttonText: '4'),
-                      ButtonText(buttonText: '5'),
-                      ButtonText(buttonText: '6'),
-                      ButtonText(buttonText: 'CE'),
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        CalcButton(
+                          onPressed: () => _pressButton('4'),
+                          buttonWidget: BaseText(baseText: '4'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('5'),
+                          buttonWidget: BaseText(baseText: '5'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('6'),
+                          buttonWidget: BaseText(baseText: '6'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('CE'),
+                          buttonWidget: BaseText(baseText: 'CE'),
+                        )
+                      ],
+                    ),
                   ),
-                  CalcRow(
-                    rowContents: [
-                      ButtonText(buttonText: '1'),
-                      ButtonText(buttonText: '2'),
-                      ButtonText(buttonText: '3'),
-                      ButtonIcon(buttonIcon: FontAwesomeIcons.equals),
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        CalcButton(
+                          onPressed: () => _pressButton('1'),
+                          buttonWidget: BaseText(baseText: '1'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('2'),
+                          buttonWidget: BaseText(baseText: '2'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('3'),
+                          buttonWidget: BaseText(baseText: '3'),
+                        ),
+                        CalcButton(
+                          onPressed: () => _pressButton('='),
+                          buttonWidget:
+                              BaseIcon(baseIcon: FontAwesomeIcons.equals),
+                        )
+                      ],
+                    ),
                   ),
                 ],
               ),
